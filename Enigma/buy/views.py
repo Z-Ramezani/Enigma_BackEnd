f70c33bf6e301e80008509959c6d32961e91528b
Zahra Ramezani <miss.ramazani@yahoo.com>
2023-04-19 15:01:53 +0430

    rewrite UserGroupBuys

--- Enigma/buy/views.py
+++ Enigma/buy/views.py
@@ -18,17 +18,22 @@ class UserGroupBuys(APIView):
         try:
             user_id = request.data.get('userID')
             group_id = request.data.get('groupID')
-            buys = buy.objects.filter(groupID=group_id, Buyers__userID=user_id)
-            serializer = BuySerializer(buys, many=True)
-            return Response(serializer.data, status=status.HTTP_200_OK)
+
+            # Get buys where the user is a buyer
+            buyer_buys = buy.objects.filter(
+                Buyer__userID=user_id, groupID=group_id).distinct()
+            buyer_serializer = BuySerializer(buyer_buys, many=True)
+
+            # Get buys where the user is a consumer
+            consumer_buys = buy.objects.filter(
+                consumer__userID=user_id, groupID=group_id).distinct()
+            consumer_serializer = BuySerializer(consumer_buys, many=True)
+
+            response_data = {
+                'buyer_buys': buyer_serializer.data,
+                'consumer_buys': consumer_serializer.data
+            }
+
+            return Response(response_data, status=status.HTTP_200_OK)
         except:
             return Response({'message': 'An error occurred.'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-            
-
-    """
-    def user_group_buys(self, request):
-        perch = buy.objects.filter( groupID=request.data['groupID'] , userID=request.data['userID'] ).values()
-        perchase = buySerializer(instance=perch, many=True)
-        return response(perchase)
-    """
