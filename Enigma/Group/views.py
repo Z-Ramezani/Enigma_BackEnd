2e7b116c79e3051734e2c8fdc34aa21f10c40d7f
Zahra Ramezani <miss.ramazani@yahoo.com>
2023-04-19 14:27:57 +0430

    rewrite GroupInfo view

--- Enigma/Group/views.py
+++ Enigma/Group/views.py
@@ -9,8 +9,9 @@ from rest_framework.response import Response
 from rest_framework import status
 from .serializers import MemberSerializer
 
+
 class ShowMembers(APIView):
- def post(self, request):
+    def post(self, request):
         try:
             group_id = request.data.get('groupID')
             group = Group.objects.get(id=group_id)
@@ -21,12 +22,19 @@ class ShowMembers(APIView):
             return Response({'message': 'Group not found.'}, status=status.HTTP_404_NOT_FOUND)
         except:
             return Response({'message': 'An error occurred.'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-class GroupInfo(APIView):
 
+
+class GroupInfo(APIView):
     def post(self, request):
-        inf = Group.objects.filter(id=request.data['id']).values()
-        info = GroupSerializer(instance=inf, many=True)
-        return Response(info.data)
+        try:
+            group_id = request.data.get('groupID')
+            group = Group.objects.get(id=group_id)
+            serializer = GroupSerializer(group)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Group.DoesNotExist:
+            return Response({'message': 'Group not found.'}, status=status.HTTP_404_NOT_FOUND)
+        except:
+            return Response({'message': 'An error occurred.'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 class DeleteGroup(APIView):
@@ -34,5 +42,3 @@ class DeleteGroup(APIView):
     def post(self, request):
         dele = Group.objects.filter(id=request.data['id']).delete()
         return Response(status=status.HTTP_200_OK)
-
-
