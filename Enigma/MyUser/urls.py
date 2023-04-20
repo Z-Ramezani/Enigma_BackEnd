db7086b9b99b4d8b7f34fd2783a274dd76698b5d
Zahra Ramezani <miss.ramazani@yahoo.com>
2023-04-20 13:03:58 +0430

    user Info

--- Enigma/MyUser/urls.py
+++ Enigma/MyUser/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, re_path
 from rest_framework.authtoken.views import obtain_auth_token
-from .views import RegisterUsers
+from .views import RegisterUsers, UserInfo
 
 urlpatterns = [
-    path('token/', obtain_auth_token),
-    path('register/', RegisterUsers.as_view()),
+    path('token', obtain_auth_token),
+    path('register', RegisterUsers.as_view()),
+    path('UserInfo', UserInfo.as_view()),
 
 ]
--- Enigma/MyUser/views.py
+++ Enigma/MyUser/views.py
@@ -5,6 +5,10 @@ from django.contrib.auth import get_user_model
 from rest_framework import permissions
 from rest_framework import generics
 from rest_framework.generics import CreateAPIView
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import MyUser
 
 class RegisterUsers(CreateAPIView):
     model = get_user_model()
@@ -12,3 +16,24 @@ class RegisterUsers(CreateAPIView):
         permissions.AllowAny
     ]
     serializer_class = MyUserSerializer
+
+
+class UserInfo(APIView):
+    def post(self, request):
+        try:
+            user_id = request.data.get('userID')
+            user = MyUser.objects.get(user_id=user_id)
+            user_info = {
+                'user_id': user.user_id,
+                'email': user.email,
+                'username': user.username,
+                'picture_id': user.picture_id,
+                'is_active': user.is_active,
+                'is_admin': user.is_admin,
+                'is_staff': user.is_staff,
+            }
+            return Response({'user_info': user_info})
+        except MyUser.DoesNotExist:
+            return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
