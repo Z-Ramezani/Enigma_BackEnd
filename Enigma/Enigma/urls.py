c92908991d771d6d3f5f1dbc702dc90ec57bf64c
Fateme Shafiee <61053360+fatemeshafiee@users.noreply.github.com>
2023-04-23 12:13:32 +0330

    swagger commented

--- Enigma/Enigma/urls.py
+++ Enigma/Enigma/urls.py
@@ -6,25 +6,25 @@ from drf_yasg import openapi
 from rest_framework import permissions
 
 
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Swagger First Blog ",
-        default_version='v1',
-        description="Test Swagger First Blog",
-        terms_of_service="https://www.ourapp.com/policies/terms/",
-        contact=openapi.Contact(email="contact@swaggerBlog.local"),
-        license=openapi.License(name="Test License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
+# schema_view = get_schema_view(
+#     openapi.Info(
+#         title="Swagger First Blog ",
+#         default_version='v1',
+#         description="Test Swagger First Blog",
+#         terms_of_service="https://www.ourapp.com/policies/terms/",
+#         contact=openapi.Contact(email="contact@swaggerBlog.local"),
+#         license=openapi.License(name="Test License"),
+#     ),
+#     public=True,
+#     permission_classes=(permissions.AllowAny,),
+# )
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('MyUser.urls')),
     path('buy/', include('buy.urls')),
     path('group/', include('Group.urls')),
-    path('', schema_view.with_ui('swagger', cache_timeout=0),
-         name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc',
-         cache_timeout=0), name='schema-redoc'),
+    # path('', schema_view.with_ui('swagger', cache_timeout=0),
+    #      name='schema-swagger-ui'),
+    # path('redoc/', schema_view.with_ui('redoc',
+    #      cache_timeout=0), name='schema-redoc'),
 ]
