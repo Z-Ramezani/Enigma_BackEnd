4f772d2decbda1f28daa17d724ff991a8a6f0afc
Fateme Shafiee <61053360+fatemeshafiee@users.noreply.github.com>
2023-04-23 11:59:31 +0330

    python anywhere as Host

--- Enigma/Enigma/settings.py
+++ Enigma/Enigma/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#&t%e9y+z*85#h3!@+r%lhvh%@($-t6vs_pi9ed#$8&)cf^%ck'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['fatemehshafiei.pythonanywhere.com']
+
 
 
 # Application definition
